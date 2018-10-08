5b9e8ec7731a4173c2a17c49d059b86f02688862
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-08 01:23:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let employees = [];
+
+function printBadges(employees) {
+  for (let index = 0; index < employees.length; index++) {
+    const name = employees[index];
+    console.log(`Welcome ${name}! You are employee #${index+1}.`);
+  }
+  return employees;
+}
+
+function tailsNeverFails() {
+  let count = 0
+
+  while (Math.random() >= 0.5) {
+    count++
+  }
+  return `You got ${count} tails in a row!`;
+}
