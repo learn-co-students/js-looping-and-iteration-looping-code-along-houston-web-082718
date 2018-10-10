60f3ae3e609af23860f9b45b0679f39de1464ee5
Flatiron School <andrewsprague@outlook.com>
2018-10-10 11:20:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`);
+    }
+    return arr;
+}
+
+function tailsNeverFails() {
+    let isTails = true;
+    let numberOfTails = 0
+    while (isTails) {
+        flip = Math.random();
+        if (flip >= 0.5) {
+            numberOfTails++;
+        } else {
+            isTails = false;
+        }
+    }
+    return `You got ${numberOfTails} tails in a row!`;
+}
